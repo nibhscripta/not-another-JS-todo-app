2cbc47293be6168dc7899a75eb39bd3a8d259ab3
tux <nibhscripta@tutanota.com>
2022-03-29 12:37:51 -0500

    migrate javascript from inline script to separate .js file

--- main.js
+++ main.js
@@ -1,2 +1,53 @@
 //code should go here but it hasn't shown up
 //aba kadabra alakzam, code appear
+let existingTodos = JSON.parse(localStorage.getItem("todos"));
+let todoData = existingTodos || [];
+todoData.forEach((todo) => {
+  addTodo(todo);
+});
+function addTodo(todoInput) {
+  let todoItem = "";
+  todoData.push(todoInput);
+  todoItem = document.createElement("p");
+  todoItem.setAttribute("class", "todo-text");
+  todoItem.setAttribute("id", "todo-text");
+  todoItem.setAttribute("onclick", "lineThrough(this)");
+  let todoText = document.createTextNode(todoInput);
+  todoItem.appendChild(todoText);
+  document.getElementById("todos-list").insertAdjacentHTML(
+    "afterbegin",
+    `
+        <div id="todo-item" class="todo-item">
+          ${todoItem.outerHTML}
+          <button id="todo-item-remove" onclick="removeTodo(this)">
+            <img src="./icons/trash.png" alt="remove" class="icon" />
+          </button>
+        </div>
+      `
+  );
+}
+document.getElementById("add-todo").onsubmit = (e) => {
+  e.preventDefault();
+  let input = e.target.input_todo.value;
+  addTodo(input);
+  document.getElementById("add-todo").reset();
+  saveTodos();
+};
+function removeTodo(todo) {
+  todo.parentNode.remove();
+  saveTodos();
+}
+function lineThrough(p) {
+  if (p.classList.contains("line-through")) {
+    p.classList.remove("line-through");
+  } else {
+    p.classList.add("line-through");
+  }
+}
+function saveTodos() {
+  todoData = [];
+  document.querySelectorAll("#todo-text").forEach((p) => {
+    todoData.push(p.innerText);
+  });
+  localStorage.setItem("todos", JSON.stringify(todoData));
+}
